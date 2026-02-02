2c1836abbe1b5f200b7feac3715339b3ccb87cf3
Kuldeep Vishwakarma <mail.kuldeepvish@gmail.com>
2026-02-02 22:25:57 +0530

    Configure CORS middleware with specific options

--- app.js
+++ app.js
@@ -45,7 +45,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().any());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin:"*",
+    methods:["GET","POST","PUT","DELETE"]
+}));
 
 // Request Logger
 // if (app.get("env") === "development") {

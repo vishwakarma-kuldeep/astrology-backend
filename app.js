319f73ee09883e7d8f810558a24d6b7be90345b3
kuldeep <mail.kuldeepvish@gmail.com>
2026-03-19 00:41:18 +0530

    new commit

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(
 
 // Request Logger
 // if (app.get("env") === "development") {
-  app.use(logger("dev"));
+app.use(logger("dev"));
 // }
 
 // Place your routes here
@@ -97,13 +97,18 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
+//   // render the error page
+//   res.status(err.status || 500);
+// });
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
 });
-
 module.exports = app;
